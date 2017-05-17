996ae38b6b7d9f848716a8e90ad7ce14e84389a4
antagme <pedroromeroaguado@gmail.com>
2017-05-17 10:37:41 +0200

    Update startup-slapd.sh

--- scripts/startup-slapd.sh
+++ scripts/startup-slapd.sh
@@ -3,7 +3,7 @@
 /usr/bin/echo "Creating DB edt.org..."
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 rm -rf /etc/openldap/slapd.d/*
-slaptest -f /opt/docker/slapd.external.conf -F /etc/openldap/slapd.d &> /dev/null
+slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d &> /dev/null
 slapadd -F /etc/openldap/slapd.d -l /opt/docker/full_edt.org.ldif &> /dev/null
 chown -R ldap.ldap /etc/openldap/slapd.d/
 chown -R ldap.ldap /var/lib/ldap/
